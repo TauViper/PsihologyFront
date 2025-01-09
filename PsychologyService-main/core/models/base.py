ae89dae6c06eaef1277b01a14892b2560dda0417
oraz aliyev <aliyev79@mail.ru>
2025-01-09 22:41:45 +0300

    main

--- PsychologyService-main/core/models/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
-from datetime import datetime
-
-
-class Base(DeclarativeBase):
-    __abstract__ = True
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now())
